f2c03765ed0633a09fbe8733deff2020bd382908
Nataliia Chorna <nataliia.chorna.9@gmail.com>
2024-02-25 17:12:19 +0200

    HW6 - Guessing game

--- src/main/java/ua/hillel/chorna/lessons/lesson6/HW6/GuessingGame.java
+++ src/main/java/ua/hillel/chorna/lessons/lesson6/HW6/GuessingGame.java
@@ -7,16 +7,20 @@ public class GuessingGame {
         Scanner scanner = new Scanner(System.in);
         int randNumber = (int) (Math.random() * 11);
         int guesses = 5;
+        boolean correctGuess = false;
         System.out.println("I picked a number between 0 and 10. Which is it? You can guess 5 times.");
         for (int i = 0; i < guesses; i++) {
             System.out.print("Enter your guess: ");
             int guess = scanner.nextInt();
             if (guess == randNumber) {
-                System.out.println("Congratulations! You won!");
+                System.out.println("Correct! You won!");
+                return;
             } else {
-                System.out.println("Wrong! Guess again!");
+                System.out.println("Wrong!");
             }
         }
-        System.out.println("You lost. The correct number was " + randNumber);
+        if (!correctGuess) {
+            System.out.println("You lost. The correct number was " + randNumber);
+        }
     }
 }
