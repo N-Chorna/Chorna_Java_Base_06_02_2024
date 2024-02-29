62a84ed0c75e37710c9bfd6156fe1bd22d7b58ae
Nataliia Chorna <nataliia.chorna.9@gmail.com>
2024-02-29 21:12:45 +0200

    Space Shuttles HW

--- src/main/java/ua/hillel/chorna/lessons/lesson6/HW6/SpaceShuttle.java
+++ src/main/java/ua/hillel/chorna/lessons/lesson6/HW6/SpaceShuttle.java
@@ -4,13 +4,12 @@ public class SpaceShuttle {
     public static void main(String[] args) {
         int counter = 0;
         for (int i = 1; counter < 100; i++) {
-            if (i / 10 == 4 || i % 10 == 4 || i / 10 == 9 || i % 10 == 9) {
+            if (i / 10 == 4 || i % 10 == 4 || i / 10 == 9 || i % 10 == 9 || i / 100 == 9 || i / 100 == 4 || i / 10 % 10 == 9 || i / 10 % 10 == 4) {
                 continue;
-            } else {
+            }
                 ++counter;
                 System.out.print(i + " ");
             }
-        }
         System.out.println("The number of shuttles is " + counter);
+        }
     }
-}
